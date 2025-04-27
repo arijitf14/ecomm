b9fb13aa9dbaf509a70ae841675581c55c6ecf3d
Arijit Bose <arijitbose@Arijits-MacBook-Air.local>
2025-04-27 17:46:09 +0530

    Initial Vite and Tailwind Setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",               // If you're using an index.html file
+    "./src/**/*.{js,ts,jsx,tsx}",  // For React components and other JS/TS files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
